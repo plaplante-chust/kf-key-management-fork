75ed2cba88bbdd3f94bded0e33e4815edbd278b7
Patrice Laplante <plaplante@ferlab.bio>
2021-02-22 16:38:02 -0500

    Fix cors to allow credentials and wild card

--- src/main/java/io/kidsfirst/config/AppConfiguration.java
+++ src/main/java/io/kidsfirst/config/AppConfiguration.java
@@ -74,7 +74,7 @@ public class AppConfiguration extends WebSecurityConfigurerAdapter implements We
         String allowedDomains = env.getProperty("application.corsAllowedDomains", "*");
         String[] allowedDomainsArr = allowedDomains.split("\\|");
         for(int i=0; i<allowedDomainsArr.length; i++){
-            config.addAllowedOrigin(allowedDomainsArr[i].trim());
+            config.addAllowedOriginPattern(allowedDomainsArr[i].trim());
         }
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
